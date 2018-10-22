61571b052ac72265bbeee52f2b7b50c461cc7bb8
Beth <bethsalvatore2018@gmail.com>
2018-10-22 13:46:02 -0400

    change to database config file

--- config/database.php
+++ config/database.php
@@ -57,11 +57,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
-            // 'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'heroku_37ef2959c0795ff'),
             'username' => env('DB+USERNAME', 'b82409b1d1c87f'),
             'password' => env('DB_PASSWORD', '82ae92e1'),
-            // 'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',

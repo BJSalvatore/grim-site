388fcc9de842e948c753aa3d29e71a6441aefc68
Beth <bethsalvatore2018@gmail.com>
2018-10-22 15:03:44 -0400

    makes changes to index.js file

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
+app.use(express.logger());
+
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-01.cleardb.net',
+  user     : 'b82409b1d1c87f',
+  password : '82ae92e1',
+  database : 'heroku_37ef2959c0795ff'
+});
+
+connection.connect();
+
+app.get('/', function(request, response) {
+  connection.query('SELECT * from posts', function(err, rows, fields) {
+      if (err) {
+        console.log('error: ', err);
+        throw err;
+      }
+      // response.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+});
+
+
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/app'));
 app.listen(port, function() {

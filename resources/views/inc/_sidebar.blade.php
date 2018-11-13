aae858dc662ff05d10bf7e135432dc0e1d9b66be
Beth <bethsalvatore2018@gmail.com>
2018-11-12 23:33:05 -0500

    fixes styling in side navbar. adds audio player to side navbar.

--- resources/views/inc/_sidebar.blade.php
+++ resources/views/inc/_sidebar.blade.php
@@ -1,21 +1,21 @@
-<div class="d-flex justify-content-center text-light bg-dark" style="margin-left: 0px; margin-top: 0px; height: 100%;">
+<div class="d-flex justify-content-center text-light bg-dark p-3" style="margin-left: 0px; margin-top: 0px; height: 100%;">
   <div class= "sidenav navbar-right">
-    <div class="d-flex flex-column col-sm-4">
-      <div class="container">
+    <div class="container" style="align-content: justify-content-center">
         <h3>New Release!</h3>
-        <img id="newLP" class="img-fluid" src="{{ asset('assets/images/TheGrimNuclearWorldOrder.jpg') }}"style="min-width: 210px; height: auto; margin-top: 0px; margin-left: 0px"></img>
-      </div>
-      <div class="container">
+        <img id="newLP" class="img-fluid mb-3 mt-3" src="{{ asset('assets/images/TheGrimNuclearWorldOrder.jpg') }}"style="min-width: 210px; height: auto;"></img>
+      <hr>
+      <h3>Cop Killer</h3>
+      <audio controls>
+        <source src="{{ asset('assets/audio/the-grim_the-grim-cop-killer-7-inch.mp3') }}" type="audio/mpeg">
+      </audio>
+      <hr>
         <h3>Upcoming Shows</h3>
         <ul>
           <li><a href="#">10/10/2018 @ This Place</a></li>
           <li><a href="#">10/22/2018 @ That Place</a></li>
           <li><a href="#">11/02/2018 @ The Other Place</a></li>
         </ul>
-      </div>
-      <div class="container">
         <img id="logo" class="img-fluid" src="{{ asset('assets/images/SoundSpeedRecordsLogo.jpg') }}" style="min-width: 210px; height: auto; margin-top: 0px; margin-left: 0px"></img>
-      </div>
     </div>
   </div>
 </div>

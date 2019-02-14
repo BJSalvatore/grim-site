54bd7a1fed05980776f9118aeb69cf2152263a2c
demoncowgirl <bethsalvatore2018@gmail.com>
2019-02-14 09:34:07 -0500

    changes return view from 'home' to '/'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     {
         $request->user()->authorizeRoles(['member', 'manager']);
 
-        return view('home');
+        return view('/');
     }
 
      // public function adminViews(Request $request)

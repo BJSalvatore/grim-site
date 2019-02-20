cd1642d0b07f0930a2b29809547fa68c10ce888f
demoncowgirl <bethsalvatore2018@gmail.com>
2019-02-20 14:03:31 -0500

    changes song title

--- resources/views/inc/_sidebar.blade.php
+++ resources/views/inc/_sidebar.blade.php
@@ -1,12 +1,12 @@
   <link href="{{ asset('../css/app.css') }}" rel="stylesheet" type="text/css">
 
-<div class="d-flex text-light bg-dark" style="height: 100%; margin-left: 0px; margin-bottom: 0px;">
-  <div class="container justify-content-center" >
+<div id="sidebar" class="d-flex text-light bg-dark align-content-center" style="height: 100%; margin-left: 0px; margin-bottom: 0px;">
+  <div class="container">
         <h3>New Release!</h3>
-        <img id="newLP" class="img-fluid p-0" src="{{ asset('assets/images/TheGrimNuclearWorldOrder.jpg') }}" style="width: 280px; height: auto;"></img>
+        <img id="newLP" class="img-fluid p-0" src="{{ asset('assets/images/TheGrimNuclearWorldOrder.jpg') }}"></img>
       <hr>
-      <h3>Cop Killer</h3>
-      <div class="d-inline-flex" style="width: 280px;">
+      <h3>Featured Song</h3>
+      <div class="embed-responsive" style="width: auto;">
         <audio controls>
           {{-- <source src="{{ asset('assets/audio/the-grim_the-grim-cop-killer-7-inch.mp3') }}" type="audio/mpeg"> --}}
         </audio>
@@ -18,8 +18,6 @@
           <li><h5><a href="#">10/22/2018 @ That Place</a><h5></li>
           <li><h5><a href="#">11/02/2018 @ The Other Place</a><h5></li>
         </ul>
-        <div class="d-inline-flex col-sm-3">
-          <img id="logo" class="d-flex img-responsive" src="{{ asset('assets/images/SoundSpeedRecordsLogo.jpg') }}" style="width: auto; height: 150px;"></img>
-        </div>
+          <img id="logo" class="d-flex img-responsive" src="{{ asset('assets/images/SoundSpeedRecordsLogo.jpg') }}"></img>
     </div>
 </div>

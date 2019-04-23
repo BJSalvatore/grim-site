22ced1e5e6601f249bd4c19f92e5ea899597534e
demoncowgirl <bethsalvatore2018@gmail.com>
2019-04-23 17:25:06 -0400

    adds gradient overlay to jumbotron

--- resources/views/inc/_jumbotron.blade.php
+++ resources/views/inc/_jumbotron.blade.php
@@ -1,11 +1,9 @@
-<div class="jumbotron jumbotron-fluid d-none d-sm-block d-md-block d-lg-block m-0 p-0">
-  <div class="row no-gutters">
-    <div class="col-md-3">
+<div class="jumbotron d-none d-sm-block d-md-block d-lg-block m-0 p-0">
+      <!-- <div class="col-md-3"> -->
         <img id="logo" src="{{ asset('assets/images/TheGrimLogoTransparent.png') }}"></img>
+      <!-- </div> -->
+
+        <img id="band-image" class="overlay img-responsive" style="background-image: url('/assets/images/MainPic.png'); background-repeat: no-repeat;"></img>
+
     </div>
-    <div class="d-flex col-md-6">
-      <div id="band-image" class="container d-flex img-responsive m-0" style="background-image: url('assets/images/MainPic.png'); background-repeat: no-repeat; height: 440px; margin-top: 0px;">
-      </div>
-    </div>
-  </div>
 </div>

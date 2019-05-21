7e19403137cc8363f83337aacf548db4978f0017
demoncowgirl <bethsalvatore2018@gmail.com>
2019-05-21 00:47:26 -0400

    updates composer.json and package.json files after update to php 7.3 and node 10.15.3

--- public/js/app.js
+++ public/js/app.js
@@ -49426,8 +49426,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/beth/grim-site/resources/assets/js/app.js */"./resources/assets/js/app.js");
-module.exports = __webpack_require__(/*! /home/beth/grim-site/resources/assets/sass/app.scss */"./resources/assets/sass/app.scss");
+__webpack_require__(/*! /home/beth/repos/grim-site/resources/assets/js/app.js */"./resources/assets/js/app.js");
+module.exports = __webpack_require__(/*! /home/beth/repos/grim-site/resources/assets/sass/app.scss */"./resources/assets/sass/app.scss");
 
 
 /***/ })

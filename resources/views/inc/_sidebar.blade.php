589263d74e158e29cfe1c4aacbf54d12d3fc507d
demoncowgirl <bethsalvatore2018@gmail.com>
2019-05-23 16:42:54 -0400

    fixes audio playback of mp3

--- resources/views/inc/_sidebar.blade.php
+++ resources/views/inc/_sidebar.blade.php
@@ -1,13 +1,15 @@
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
 <div id="sidebar" class="container-fluid text-center text-light bg-dark d-none d-sm-block d-md-block d-lg-block">
 
         <div class=" col justify-content-center">
               <p>New Release!</p>
               <img id="newLP" class="img-responsive" src="{{ asset('assets/images/TheGrimNuclearWorldOrder.jpg') }}"></img>
-          <p>Song Title</p>
+          <p>Cop Killer</p>
           <div id="audio" class="d-flex text-center">
             <audio controls class="embed-responsive">
-              <source src="{{ asset('public/audio/the-grim-cop-killer-7-inch.mp3') }}">
-              <source src="{{ asset('public/audio/the-grim-cop-killer-7-inch.ogg') }}">
+                <source id="audioSource" type="audio/mp3" src="{{ secure_asset('https://s3.amazonaws.com/grim-images/audio/the-grim_the-grim-cop-killer-7-inch.mp3')}}">
+                <script src="https://sdk.amazonaws.com/js/aws-sdk-2.410.0.min.js"></script>
             </audio>
           </div>
         </div>

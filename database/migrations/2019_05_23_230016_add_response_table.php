8d7cb36e154f241180892d45621e156249f24688
demoncowgirl <bethsalvatore2018@gmail.com>
2019-05-24 14:07:34 -0400

    more work on media queries.

--- database/migrations/2019_05_23_230016_add_response_table.php
+++ database/migrations/2019_05_23_230016_add_response_table.php
@@ -1,11 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 use App\Messages;
-
 class AddResponseTable extends Migration
 {
     /**
@@ -15,29 +12,19 @@ class AddResponseTable extends Migration
      */
     public function up()
     {
-
         Schema::create('comments', function (Blueprint $table) {
         $table->increments('id')->unsigned();
         $table->integer('message_id')->unsigned();
         $table->string('response');
         $table->string('respondee');
         $table->timestamp('responded_on')-> nullable();
-
         $table->foreign('message_id')
         ->references('id')
         ->on('messages')
         ->onDelete('cascade');
-
       });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+     // Reverse the migrations.
     public function down()
-    {
           Schema::dropIfExists('messages');
-    }
 }
--- resources/views/inc/_sidebar.blade.php
+++ resources/views/inc/_sidebar.blade.php
@@ -1,8 +1,6 @@
-<meta name="csrf-token" content="{{ csrf_token() }}">
-
-<div id="sidebar" class="container-fluid text-center text-light bg-dark d-none d-sm-block d-md-block d-lg-block">
-
-        <div class=" col justify-content-center">
+<div id="sidebar" class="container text-center text-light bg-dark d-none d-sm-block d-md-block d-lg-block">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+        <div class="col justify-content-center">
               <p>New Release!</p>
               <img id="newLP" class="img-responsive" src="{{ asset('assets/images/TheGrimNuclearWorldOrder.jpg') }}"></img>
           <p>Cop Killer</p>
@@ -13,14 +11,12 @@
             </audio>
           </div>
         </div>
-
-        <div id="calendar" class="col mb-3">
-        <p>Upcoming Shows</p>
-          <div class="embed-responsive justify-content-center" data-tockify-component="mini" data-tockify-calendar="thegrimband"></div>
-          <script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
+        <div class="col justify-content-center">
+            <p>Upcoming Shows</p>
+              <div id="calendar" class="embed-responsive justify-content-center" data-tockify-component="mini" data-tockify-calendar="thegrimband"></div>
+              <script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
         </div>
-
-        <div id="recordLabelLogo" class="col-sm d-none d-sm-none d-md-none d-lg-block d-xl-block">
-            <img class="img-responsive mb-3" src="{{ asset('assets/images/SoundSpeedRecordsLogo.jpg') }}"></img>
+        <div class="col-sm d-none d-sm-none d-md-block d-lg-block d-xl-block">
+            <img id="recordLabelLogo" class="img-responsive mt-3 mb-3" src="{{ asset('assets/images/SoundSpeedRecordsLogo.jpg') }}"></img>
         </div>
   </div>

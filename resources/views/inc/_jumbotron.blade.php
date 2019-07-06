70f304a90a2e9d239762fe39b2a7def7a8c43ed6
demoncowgirl <bethsalvatore2018@gmail.com>
2019-07-06 18:07:54 -0400

    more work on mobile responsiveness

--- resources/views/inc/_jumbotron.blade.php
+++ resources/views/inc/_jumbotron.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <div class="jumbotron d-none col-sm d-none d-md-block d-lg-block d-xl-block m-0 p-0">
+  <div class="jumbotron d-none d-sm-block d-md-block d-lg-block d-xl-block m-0 p-0">
           <img id="logo" src="{{ asset('images/TheGrimLogoTransparent.png') }}"></img>
           <img id="band-image" class="overlay img" style="background-image: url('/images/MainPic.png'); background-repeat: no-repeat;"></img>
 </div>
--- resources/views/inc/_sidebar.blade.php
+++ resources/views/inc/_sidebar.blade.php
@@ -1,12 +1,10 @@
 <div class="px-3">
   <div id="sidebar" class="mx-auto text-center text-light bg-dark d-none d-sm-block d-md-block d-lg-block">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-  <div class="col">
     <div class="container-fluid mt-2">
-      <p>New Release!</p>
+      <div class="col">
+        <p>New Release!</p>
         <img id="newLP" class="img-responsive" src="{{ asset('images/TheGrimNuclearWorldOrder.jpg') }}"></img>
-    </div>
-    <div class="container-fluid mt-3">
       <center><p>Cop Killer</p></center>
       <div id="audio" class="d-flex text-center">
         <audio controls class="embed-responsive">
@@ -16,17 +14,13 @@
       </div>
     </div>
   </div>
-  <div class="col">
     <div class="container-fluid text-center justify-content-center mt-3">
-      <p>Upcoming Shows</p>
       <div class="col">
-        <div class="row">
-          <div id="calendar" class="embed-responsive justify-content-center mb-3" data-tockify-height="448px" data-tockify-component="mini" data-tockify-calendar="thegrimband"></div>
+      <p>Upcoming Shows</p>
+          <div id="calendar" class="embed-responsive justify-content-center" data-tockify-component="mini" data-tockify-calendar="thegrimband"></div>
           <script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
-        </div>
       </div>
     </div>
-  </div>
       <div class="col-sm d-none d-md-block d-lg-block d-xl-block">
           <img id="recordLabelLogo" class="img-responsive mt-3 mb-3" src="{{ asset('images/SoundSpeedRecordsLogo.jpg') }}"></img>
       </div>

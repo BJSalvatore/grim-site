60ce956064fc63e7fda0cc6464d3c5fa3bf1ba13
demoncowgirl <bethsalvatore2018@gmail.com>
2019-08-27 10:32:03 -0500

    more updates to calendar

--- resources/views/inc/_sidebar.blade.php
+++ resources/views/inc/_sidebar.blade.php
@@ -17,8 +17,7 @@
     <div class="container-fluid text-center justify-content-center mt-3">
       <div class="col">
       <p>Upcoming Shows</p>
-      <div id="calendar" class="embed-responsive justify-content-center mb-1" data-tockify-component="mini" data-tockify-calendar="thegrimband"></div>
-        <script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
+      <div id="calendar" class="embed-responsive justify-content-center mb-1" data-tockify-component="calendar" data-tockify-calendar="thegrimband"></div>
       </div>
     </div>
       <div class="col-sm d-none col-md-block d-lg-block d-xl-block">
